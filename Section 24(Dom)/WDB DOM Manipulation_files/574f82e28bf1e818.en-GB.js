bd05edf8c9342f2084d10fe34aaa44c8b7bcf2c8
Shahnawaj Alam <109065642+shahnawaj786@users.noreply.github.com>
2023-05-22 19:10:42 +0530

    Add files via upload

---
+++ Section 24(Dom)/WDB DOM Manipulation_files/574f82e28bf1e818.en-GB.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["en-GB"] = Object.assign(window['cmsg']['assets']["en-GB"] || {}, {"4204":{"js":["0bb85d405e94b942.strings.js"],"css":[]},"6887":{"js":["6d2377a0930cb6c2.strings.js"],"css":[]},"9427":{"js":["ca09d425bd712e9c.strings.js"],"css":[]}});
